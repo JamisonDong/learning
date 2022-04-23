32f11e8c404abb0766bb1f608803a2741ef6346a
dongxiaokui <1014236121@qq.com>
2022-04-23 16:32:26 +0800

    rollup

---
+++ 2-PartⅡ/12-rollup-demo/03-plugins/rollup.config.js
@@ -0,0 +1,13 @@
+import json from 'rollup-plugin-json'
+
+export default {
+  input: 'src/index.js',
+  output: {
+    file: 'dist/bundle.js',
+    format: 'iife'
+  },
+  plugins: [
+    // 将调用结果放置在此
+    json()
+  ]
+}
