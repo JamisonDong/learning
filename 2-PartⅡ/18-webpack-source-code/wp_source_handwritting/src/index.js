de151627479efb6c5c93077ed77c026ce1c06a14
董建成 <1014236121@qq.com>
2022-04-26 16:01:05 +0800

    wp-source-code-handwriting

---
+++ 2-PartⅡ/18-webpack-source-code/wp_source_handwritting/src/index.js
@@ -0,0 +1,8 @@
+import name, { age } from './login.js'
+
+// let name = require('./login')
+
+console.log('index.js 执行了')
+
+console.log(name, '<------')
+console.log(age, '<------')
---
+++ 2-PartⅡ/18-webpack-source-code/wp_source_handwritting/src/login.js
@@ -0,0 +1,4 @@
+// module.exports = 'hello world'
+
+export default 'zce是一个帅哥'
+export const age = 40
