1c27208656e4fa4e7e29a50bd7a1ed4ebd115bdd
dongxiaokui <1014236121@qq.com>
2022-05-06 15:06:25 +0800

    为DOM元素添加属性

---
+++ 3-PartⅢ/01/02-Virtual DOM 及 Diff 算法/tiny-react/src/TinyReact/updateNodeElement.js
@@ -0,0 +1,23 @@
+export default function updateNodeElement (newElement, virtualDOM) {
+  // 获取节点对应的属性对象
+  const newProps = virtualDOM.props
+  Object.keys(newProps).forEach(propName => {
+    // 获取属性值
+    const newPropsValue = newProps[propName]
+    // 判读属性是否是时间属性 onClick -> click
+    if (propName.slice(0, 2) === 'on') {
+      // 时间名称
+      const eventName = propName.toLowerCase().slice(2)
+      // 为元素添加事件
+      newElement.addEventListener(eventName, newPropsValue)
+    } else if (propName === 'value' || propName === 'checked') {
+      newElement[propName] = newPropsValue
+    } else if (propName !== 'children') {
+      if (propName === 'className') {
+        newElement.setAttribute('class', newPropsValue)
+      } else {
+        newElement.setAttribute(propName, newPropsValue)
+      }
+    }
+  })
+};
