7e9144131c5c7c171e3584f791cf5e90c5caba3b
dongxiaokui <1014236121@qq.com>
2022-05-07 15:34:09 +0800

    diff 删除节点

--- 3-PartⅢ/01/02-Virtual DOM 及 Diff 算法/tiny-react/src/TinyReact/diff.js
+++ 3-PartⅢ/01/02-Virtual DOM 及 Diff 算法/tiny-react/src/TinyReact/diff.js
@@ -1,13 +1,22 @@
+import createDOMElement from "./createDOMElement"
 import mountElement from "./mountElement"
 import updateNodeElement from "./updateNodeElement"
 import updateTextNode from "./updateTextNode"
+import unmountNode from "./unmountNode"
 
 export default function diff (virtualDOM, container, oldDOM) {
   const oldVirtualDOM = oldDOM && oldDOM._virtualDOM
   // 判断oldDOM是否存在
   if (!oldDOM) {
     mountElement(virtualDOM, container)
-  } else if (oldVirtualDOM && virtualDOM.type === oldVirtualDOM.type) {
+  }
+  // 类型不同 则替换 
+  else if (virtualDOM.type !== oldVirtualDOM.type && typeof virtualDOM !== 'function') {
+    const newElement = createDOMElement(virtualDOM)
+    oldDOM.parentNode.replaceChild(newElement, oldDOM)
+  }
+  // 类型相同 更新
+  else if (oldVirtualDOM && virtualDOM.type === oldVirtualDOM.type) {
     if (virtualDOM.type === 'text') {
       // 更新内容
       updateTextNode(virtualDOM, oldVirtualDOM, oldDOM)
@@ -15,9 +24,21 @@ export default function diff (virtualDOM, container, oldDOM) {
       // 更新元素属性
       updateNodeElement(oldDOM, virtualDOM, oldVirtualDOM)
     }
-
+    // 对比子节点
     virtualDOM.children.forEach((child, i) => {
       diff(child, oldDOM, oldDOM.childNodes[i])
     })
+    // 删除节点
+    // 获取旧节点
+    let oldChildNodes = oldDOM.childNodes
+    // 判断旧节点的数量
+
+    if (oldChildNodes.length > virtualDOM.children.length) {
+      // 有节点需要被删除
+      for (let i = oldChildNodes.length - 1; i > virtualDOM.children.length - 1; i--) {
+        unmountNode(oldChildNodes[i])
+      }
+    }
+
   }
 };
