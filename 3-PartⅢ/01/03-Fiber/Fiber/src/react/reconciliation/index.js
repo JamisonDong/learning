53d70e367e850724a3b5b405839afbe76ff7615d
dongxiaokui <1014236121@qq.com>
2022-05-09 16:20:38 +0800

    taskQueue

---
+++ 3-PartⅢ/01/03-Fiber/Fiber/src/react/reconciliation/index.js
@@ -0,0 +1,22 @@
+import { createTaskQueue } from "../Misc"
+
+
+const taskQueue = createTaskQueue()
+
+export const render = (element, dom) => {
+  /**
+   * 1.向任务队列中添加任务
+   * 2.指定在浏览器空闲时执行任务
+   */
+
+
+  /**
+   * 任务就是通过 vdom 对象 构建fiber对象
+   */
+  taskQueue.push({
+    dom,
+    props: { children: element }
+  })
+
+  console.log(taskQueue.pop());
+}
