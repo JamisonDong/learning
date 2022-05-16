d25bb927b55caf4f29546ce29aaaadbb055ea83a
dongxiaokui <1014236121@qq.com>
2022-05-16 14:34:32 +0800

    代码重构

--- 3-PartⅢ/02-React数据流方案/01-Redux/code/react-redux-guide/src/index.js
+++ 3-PartⅢ/02-React数据流方案/01-Redux/code/react-redux-guide/src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 // import App from './App';
-import { createStore } from 'redux'
+
 import Counter from './components/Counter';
 import { Provider } from 'react-redux';
-
+import { store } from "./store"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
@@ -13,26 +13,6 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
  * Provider
  * connect
  */
-
-const initialState = {
-  count: 0
-}
-
-function reducer (state = initialState, action) {
-  switch (action.type) {
-    case 'increment':
-      return { count: state.count + 1 }
-    case 'decrement':
-      return { count: state.count - 1 }
-    default:
-      return state
-  }
-}
-const store = createStore(reducer)
-
-// const increment = { type: 'increment' }
-// const decrement = { type: 'decrement' }
-
 root.render(
   // 通过Provider 将store放在了全局的组件可以访问到的地方
   <Provider store={store}><Counter /></Provider>
---
+++ 3-PartⅢ/02-React数据流方案/01-Redux/code/react-redux-guide/src/store/reducers/counter.reducer.js
@@ -0,0 +1,17 @@
+import { DECREMENT, INCREMENT } from "../constant/counter.const"
+
+const initialState = {
+  count: 0
+}
+
+/* eslint import/no-anonymous-default-export: [2, {"allowArrowFunction": true}] */
+export default (state = initialState, action) => {
+  switch (action.type) {
+    case INCREMENT:
+      return { count: state.count + 1 }
+    case DECREMENT:
+      return { count: state.count - 1 }
+    default:
+      return state
+  }
+}
