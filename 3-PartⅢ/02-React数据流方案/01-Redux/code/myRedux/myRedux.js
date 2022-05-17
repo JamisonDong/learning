155ec9b12e25f89a2c781a268edbe60025b9f518
dongxiaokui <1014236121@qq.com>
2022-05-17 19:02:29 +0800

    参数类型约束

--- 3-PartⅢ/02-React数据流方案/01-Redux/code/myRedux/myRedux.js
+++ 3-PartⅢ/02-React数据流方案/01-Redux/code/myRedux/myRedux.js
@@ -5,6 +5,8 @@
  */
 
 function createStore (reducer, preloadedState) {
+  // 约束reducer参数类型
+  if (typeof reducer !== 'function') throw new Error('reducer is not a function')
   // store对象中存储的状态
   var currentState = preloadedState
 
@@ -18,6 +20,12 @@ function createStore (reducer, preloadedState) {
 
   // 触发action
   function dispatch (action) {
+    // 判断action是否是对象
+    if (!isPlainObject(action)) {
+      throw new Error('action is not a object')
+    }
+    // 判断action中是否有type属性
+    if (typeof action.type === 'undefined') throw new Error('action need a attr of type')
     currentState = reducer(currentState, action)
     // 循环数组 调用订阅者
     for (var i = 0; i < currentListeners.length; i++) {
@@ -38,4 +46,16 @@ function createStore (reducer, preloadedState) {
     dispatch,
     subscribe
   }
+}
+
+// 判断参数是否为对象
+function isPlainObject (obj) {
+  // 排除基本数据类型和null
+  if (typeof obj !== 'object' || obj === null) return false
+  // 区分数组和对象 原型对象对比
+  var proto = obj
+  while (Object.getPrototypeOf(proto) !== null) {
+    proto = Object.getPrototypeOf(proto)
+  }
+  return proto === Object.getPrototypeOf(obj)
 }
