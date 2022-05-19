5d99ec09fc6fce0fe06cda5efacbcab02cfb07c5
dongxiaokui <1014236121@qq.com>
2022-05-19 18:48:19 +0800

    styled

--- 3-PartⅢ/03-React Hooks、Chakra-UI、组件性能优化、封装组件库/03-CSS-IN-JS/code/emotion-guide/src/App.js
+++ 3-PartⅢ/03-React Hooks、Chakra-UI、组件性能优化、封装组件库/03-CSS-IN-JS/code/emotion-guide/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { css } from '@emotion/core';
 import { useTheme } from 'emotion-theming';
 import Css from './Css';
+import styled from "@emotion/styled"
 
 // const primaryColor = props => css`
 //   color: ${props.colors.primary};
@@ -10,19 +11,37 @@ import Css from './Css';
 //   background: skyblue;
 // `
 
+const Button = styled.button`
+  width: 100px;
+  height: 30px;
+  background: ${props => props.bgColor || "skyblue"};
+
+`
+
+const Container = styled.div({
+  width: 800,
+  background: 'pink'
+}, props => ({
+  width: props.w || 1000,
+  background: "rgba(139, 170, 65, 1)",
+  margin: '0 auto'
+}))
 
 const primaryColor = props => css({
   color: props.colors.primary,
   // width: 100,
   // height: 100,
-  background: 'pink'
+  // background: 'pink'
 })
 
 function App () {
   console.log(useTheme());
-  return <div css={primaryColor}>
+  return <Container
+    w={1600}
+    css={primaryColor}>
     <Css css={primaryColor} />
-  </div>;
+    <Button bgColor="blue">我是杨可爱</Button>
+  </Container>;
 }
 
 
