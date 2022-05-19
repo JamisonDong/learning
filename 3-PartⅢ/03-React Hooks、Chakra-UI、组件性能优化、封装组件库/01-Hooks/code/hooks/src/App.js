abf2552a4171577cb04e0c4c5160b61ff0c36264
dongxiaokui <1014236121@qq.com>
2022-05-19 09:45:15 +0800

    useReducer

--- 3-PartⅢ/03-React Hooks、Chakra-UI、组件性能优化、封装组件库/01-Hooks/code/hooks/src/App.js
+++ 3-PartⅢ/03-React Hooks、Chakra-UI、组件性能优化、封装组件库/01-Hooks/code/hooks/src/App.js
@@ -1,27 +1,46 @@
-import React, { useState } from 'react';
+import React, { /* useState, */ useReducer } from 'react';
 
 
 
 function App () {
-  const [count, setCount] = useState(0);
-  const [person, setPerson] = useState({ name: "张三", age: 20 })
+  // const [count, setCount] = useState(0);
+  // const [person, setPerson] = useState({ name: "张三", age: 20 })
 
-  function handleCount () {
-    setCount((count) => {
-      const newCount = count + 100
-      document.title = newCount
-      return newCount
-    })
+  // function handleCount () {
+  //   setCount((count) => {
+  //     const newCount = count + 100
+  //     document.title = newCount
+  //     return newCount
+  //   })
+  // }
 
+  function reducer (state, action) {
+    switch (action.type) {
+      case 'increment':
+        return state + 1
+      case 'decrement':
+        return state - 1
+      default:
+        return state
+    }
   }
+
+  const [count, dispatch] = useReducer(reducer, 0)
 
   return <div>
     <p>{count}</p>
-    <p>{person.name}</p>
-    <p>{person.age}</p>
-    <button onClick={handleCount}>+1</button>
-    <button onClick={() => setPerson({ name: "李四", age: 30 })}>setPerson</button>
+    {/* <p>{person.name}</p>
+    <p>{person.age}</p> */}
+    {/* <button onClick={handleCount}>+1</button> */}
+    {/* <button onClick={() => setPerson({ name: "李四", age: 30 })}>setPerson</button> */}
+
+    <button onClick={() => dispatch({ type: 'increment' })}>+1</button>
+    <button onClick={() => dispatch({ type: 'decrement' })}>-1</button>
   </div>;
+}
+
+function Foo () {
+  return <div>Foo组件</div>
 }
 
 export default App;
