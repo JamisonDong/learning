6bfcc609452ae72aaae15ccc8bae7822df27c311
dongxiaokui <1014236121@qq.com>
2022-05-20 18:52:23 +0800

    将启动服务器代码和渲染代码进行模块化拆分

--- 3-PartⅢ/04-React服务端渲染/01-ReactSSR/code/react-ssr/src/server/index.js
+++ 3-PartⅢ/04-React服务端渲染/01-ReactSSR/code/react-ssr/src/server/index.js
@@ -1,22 +1,6 @@
-import React from "react";
 import app from "./http";
-import { renderToString } from "react-dom/server"
-import Home from "../share/pages/Home"
+import renderer from "./renderer"
 
 app.get("/", (req, res) => {
-  const content = renderToString(<Home />)
-
-  res.send(`
-    <html>
-      <head>
-        <title>
-          React SSR
-        </title>
-      </head>
-      <body>
-        <div id="root">${content}</div>
-        <script src="bundle.js"></script>
-      </body>
-    </html>
-  `)
+  res.send(renderer())
 })
