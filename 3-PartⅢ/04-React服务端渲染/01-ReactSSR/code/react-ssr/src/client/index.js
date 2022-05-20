66421e2aeb60e8575e0f7190d000f642567b233d
dongxiaokui <1014236121@qq.com>
2022-05-20 19:23:22 +0800

    路由支持

--- 3-PartⅢ/04-React服务端渲染/01-ReactSSR/code/react-ssr/src/client/index.js
+++ 3-PartⅢ/04-React服务端渲染/01-ReactSSR/code/react-ssr/src/client/index.js
@@ -1,6 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Home from "../share/pages/Home";
+import { BrowserRouter } from "react-router-dom"
+import { renderRoutes } from "react-router-config"
+import routes from "../share/routes"
 
-
-ReactDOM.hydrate(<Home />, document.getElementById("root"))
+ReactDOM.hydrate(
+  <BrowserRouter>
+    {renderRoutes(routes)}
+  </BrowserRouter>,
+  document.getElementById("root"))
---
+++ 3-PartⅢ/04-React服务端渲染/01-ReactSSR/code/react-ssr/src/share/pages/List.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function List () {
+  return (
+    <div>
+      List working
+    </div>
+  )
+}
+
+export default List
+
