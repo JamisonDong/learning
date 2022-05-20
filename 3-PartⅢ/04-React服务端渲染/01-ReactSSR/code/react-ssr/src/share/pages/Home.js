4a0b9a198b2dccd4e6caea68fa9459893b52aea3
dongxiaokui <1014236121@qq.com>
2022-05-20 19:28:01 +0800

    客户端路由支持

--- 3-PartⅢ/04-React服务端渲染/01-ReactSSR/code/react-ssr/src/share/pages/Home.js
+++ 3-PartⅢ/04-React服务端渲染/01-ReactSSR/code/react-ssr/src/share/pages/Home.js
@@ -1,8 +1,11 @@
 import React from "react"
+import { Link } from "react-router-dom"
 
 function Home () {
   return <div onClick={() => console.log("hello")}>
     Home Works
+
+    <Link to="/list"> 跳转到List</Link>
   </div>
 }
 
